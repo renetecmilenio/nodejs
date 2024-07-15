72ea94bffe7ad8a040902e926670e7bdd0e528fd
casas-javer <139288692+casas-javer@users.noreply.github.com>
2024-07-15 21:40:51 +0000

    cambio a correo de christian

--- index.js
+++ index.js
@@ -42,10 +42,10 @@ app.post("/enviar-correo", (req, res) => {
   // Configurar los datos del correo
   let mailOptions = {
     from: "noreply@javer.com.mx", // Cambiar por tu dirección de correo
-    to: "rceron@javer.com.mx", // Dirección de correo a la que se enviará el formulario
+    to: "ctrevinov@javer.com.mx", // Dirección de correo a la que se enviará el formulario
     subject: "Nuevo mensaje Sugerencia / recomendación",
     text: `Nombre: ${nombre}\nTeléfono: ${telefono}\nEmail: ${email}\nDesarrollo: ${desarrollo}\nMensaje: ${mensaje}`,
-    bcc: 'reno7882@gmail.com' // Dirección de correo en copia oculta (BCC)
+    bcc: 'rceron@javer.com.mx' // Dirección de correo en copia oculta (BCC)
   };
 
   // Enviar el correo electrónico
