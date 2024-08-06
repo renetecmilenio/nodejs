fa118eb11f3cb41f3e8258e22a15419e03c6aded
casas-javer <139288692+casas-javer@users.noreply.github.com>
2024-08-06 16:58:54 +0000

    primer commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Configuración de CORS
-const whitelist = ['https://javer.com.mx', 'https://www.javer.com.mx', 'https://buzonjaver.com'];
+const whitelist = ['https://javer.com.mx', 'https://www.javer.com.mx', 'http://localhost'];
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
@@ -25,7 +25,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Ruta para manejar el envío del formulario
-app.post("/enviar-correo", (req, res) => {
+app.post("/desarrollo-fuerte", (req, res) => {
   const { nombre, telefono, email, desarrollo, mensaje } = req.body;
 
   // Configurar el transporte del correo electrónico usando nodemailer
@@ -33,7 +33,7 @@ app.post("/enviar-correo", (req, res) => {
     service: "gmail",
     auth: {
     user: "grupojaver@gmail.com", //  Cambiar por tu dirección de correo desde donde se enviarán los correos
-      pass: "okjy snbu tcks xavp",  // Cambiar por la contraseña de tu correo
+    pass: "okjy snbu tcks xavp",  // Cambiar por la contraseña de tu correo
      // user: "noreply@javer.com.mx",  Cambiar por tu dirección de correo desde donde se enviarán los correos
     //  pass: "N0r3ply2020M",  Cambiar por la contraseña de tu correo
     },
@@ -44,7 +44,7 @@ app.post("/enviar-correo", (req, res) => {
     from: "noreply@javer.com.mx", // Cambiar por tu dirección de correo
     to: "ctrevinov@javer.com.mx", // Dirección de correo a la que se enviará el formulario
     subject: "Nuevo mensaje Sugerencia / recomendación",
-    text: `Nombre: ${nombre}\nTeléfono: ${telefono}\nEmail: ${email}\nDesarrollo: ${desarrollo}\nMensaje: ${mensaje}`,
+    text: `Nombre: ${nombre}\nTeléfono: ${telefono}\nEmail: ${email}\nDesarrollo: ${desarrollo}`,
     bcc: 'rceron@javer.com.mx' // Dirección de correo en copia oculta (BCC)
   };
 
@@ -57,7 +57,7 @@ app.post("/enviar-correo", (req, res) => {
       console.log("Correo enviado: %s", info.response);
       // res.status(200).send("Correo enviado correctamente");
           // Redirigir al usuario a la página de gracias
-    res.redirect('https://javer.com.mx/sugerencias-gracias');
+    res.redirect('https://javer.com.mx/gracias');
     }
   });
 });
